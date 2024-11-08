15c8592f45f687c4284eb127be9441aede5298d6
Koen Bloemen <koen.bloemen@pxl.be>
2024-11-08 10:03:11 +0100

    Deel1

--- Labo-11---Arrays/MainWindow.xaml.cs
+++ Labo-11---Arrays/MainWindow.xaml.cs
@@ -20,5 +20,31 @@ namespace Labo_11___Arrays
         {
             InitializeComponent();
         }
+
+        private void outputButton_Click(object sender, RoutedEventArgs e)
+        {
+            int smallest = int.MaxValue;
+            int biggest = int.MinValue;
+
+            //int[] getallen = new int[6] { 100, 50, 20, 60, 90, 80 };
+            int[] getallen = { 100, 50, 20, 60, 90, 80 };
+            StringBuilder sbOutput = new StringBuilder();
+            sbOutput.Append($"In totaal zijn er {getallen.Length} getallen: ");
+            for (int i = 0; i < getallen.Length; i++)
+            {
+                if (getallen[i] > biggest)
+                {
+                    biggest = getallen[i];
+                }
+                if (getallen[i] < smallest)
+                {
+                    smallest = getallen[i];
+                }
+            }
+            sbOutput.AppendLine();
+            sbOutput.AppendLine($"Kleinste getal = {smallest}");
+            sbOutput.AppendLine($"Grootste getal = {biggest}");
+            outputTextBox.Text = sbOutput.ToString();
+        }
     }
 }
